e5ed18d74d8978443183c4598930993ba94ea965
justin-10j <justin1998john@gmail.com>
2026-02-08 22:40:30 +0000

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,14 +1,13 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.5/firebase-app.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.5/firebase-firestore.js";
 
-// 🔥 Paste your Firebase config here (Firebase Console → Project settings → Your apps → Web app)
 const firebaseConfig = {
-  apiKey: "PASTE_HERE",
-  authDomain: "PASTE_HERE",
-  projectId: "PASTE_HERE",
-  storageBucket: "PASTE_HERE",
-  messagingSenderId: "PASTE_HERE",
-  appId: "PASTE_HERE"
+  apiKey: "AIzaSyCtyKQPqjVd2BJo5NM9nUgGdp7MYct5f0w",
+  authDomain: "wedding-invitation-49fab.firebaseapp.com",
+  projectId: "wedding-invitation-49fab",
+  storageBucket: "wedding-invitation-49fab.firebasestorage.app",
+  messagingSenderId: "411590759614",
+  appId: "1:411590759614:web:7646e3fddde6f961eafaef"
 };
 
 export const app = initializeApp(firebaseConfig);
